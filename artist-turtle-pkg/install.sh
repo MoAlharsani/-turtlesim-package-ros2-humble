346181fdda28123af1a78fba07cdd03d7c8187be
Mohammed Alharbi <86081496+MoAlharsani@users.noreply.github.com>
2024-07-07 13:24:02 +0300

    Update install.sh

--- artist-turtle-pkg/install.sh
+++ artist-turtle-pkg/install.sh
@@ -19,10 +19,10 @@ cd artist_turtle_controller
 
 # Remove existing setup.py and download new one
 rm -f setup.py
-curl -O https://raw.githubusercontent.com/MoAlharsani/turtlesim-package-ros2-humble/main/go-to-coordinate/setup.py
+curl -O https://raw.githubusercontent.com/MoAlharsani/turtlesim-package-ros2-humble/main/artist-turtle-pkg/setup.py
 
 # Download go_to_coordinate.py and make it executable
-curl -O https://raw.githubusercontent.com/MoAlharsani/turtlesim-package-ros2-humble/main/go-to-coordinate/go_to_coordinate.py
+curl -O https://raw.githubusercontent.com/MoAlharsani/turtlesim-package-ros2-humble/main/artist-turtle-pkg/go_to_coordinate.py
 chmod +x go_to_coordinate.py
 
 # Navigate back to the workspace root and build the workspace
